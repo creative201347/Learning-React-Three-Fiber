35a9d66863647cc790b5ab89c620e7e93f6603e1
creative201347 <creative201347@gmail.com>
2022-12-28 06:52:24 +0545

    feat: light and shadow

--- README.md
+++ README.md
@@ -19,4 +19,39 @@
 <OrbitControls ref={orbitControlsRef} />
 <mesh >...</mesh>
 ```
-![screenshot](public/screenshots/screen.gif)  
+![](public/screenshots/screen_01.gif)
+
+
+## Lights and Shadows
+```js
+ <Canvas shadows>
+  <mesh position={[0, 0.5, 0]} castShadow>
+    <sphereGeometry args={[0.5, 32, 32]} />
+    <meshStandardMaterial color="#ffffff" metalness={0.6} roughness={0.2} />
+  </mesh>
+
+  <mesh rotation={[-angleToRadians(90), 0, 0]} receiveShadow>
+    <planeGeometry args={[20, 20]} />
+    <meshStandardMaterial color="#1ea3d8" />
+  </mesh>
+
+  <ambientLight args={["#ffffff", 0.25]} />
+
+  <spotLight
+    args={["#ffffff", 1.5, 7, angleToRadians(45), 0.4]}
+    position={[-3, 1, 0]}
+    castShadow
+  />
+</Canvas>
+```
+
+## Enviroment
+```js
+<Environment background>
+  <mesh>
+      <sphereGeometry args={[50, 100, 100]} />
+        <meshBasicMaterial side={THREE.BackSide} color="#2266cc" />
+  </mesh>
+</Environment>
+```
+![](public/screenshots/shadows_02.gif)
--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -1,37 +1,41 @@
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 import { angleToRadians } from "../utils";
+import * as THREE from "three";
 
 const Three = () => {
-  // Code to move the camera around
-  const orbitControlsRef = useRef(null);
-
-  useFrame((state) => {
-    if (!!orbitControlsRef.current) {
-      const { x, y } = state.mouse;
-      orbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(45));
-      orbitControlsRef.current.setPolarAngle((y + 1) * angleToRadians(90 - 30));
-      orbitControlsRef.current.update();
-    }
-  });
-  useEffect(() => {}, [orbitControlsRef.current]);
-
   return (
     <>
       <PerspectiveCamera makeDefault position={[0, 1, 5]} />
-      <OrbitControls ref={orbitControlsRef} />
+      <OrbitControls />
 
-      <mesh position={[0, 0.5, 0]}>
+      <mesh position={[0, 0.5, 0]} castShadow>
         <sphereGeometry args={[0.5, 32, 32]} />
-        <meshStandardMaterial color="#ffffff" />
+        <meshStandardMaterial color="#ffffff" metalness={0.6} roughness={0.2} />
       </mesh>
 
-      <mesh rotation={[-angleToRadians(90), 0, 0]}>
-        <planeGeometry args={[7, 7]} />
+      <mesh rotation={[-angleToRadians(90), 0, 0]} receiveShadow>
+        <planeGeometry args={[20, 20]} />
         <meshStandardMaterial color="#1ea3d8" />
       </mesh>
-      <ambientLight args={["#ffffff", 1]} />
+
+      <ambientLight args={["#ffffff", 0.25]} />
+
+      <spotLight
+        args={["#ffffff", 1.5, 7, angleToRadians(45), 0.4]}
+        position={[-3, 1, 0]}
+        castShadow
+      />
+
+      <Environment background>
+        <mesh>
+          <sphereGeometry args={[50, 100, 100]} />
+          <meshBasicMaterial side={THREE.BackSide} color="#2266cc" />
+        </mesh>
+      </Environment>
     </>
   );
 };
